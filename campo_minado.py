e0e9f444fc8a6e02238d3162193b7d33c7a424d9
Dojo People <dojo@thoughtworks.com>
2016-01-20 20:36:23 -0200

    Adding test with two points.

--- campo_minado.py
+++ campo_minado.py
@@ -1,4 +1,8 @@
 def resolve_campo_minado(campo_minado):
+    if len(campo_minado[0]) == 2:
+        if campo_minado[0][0] == ".":
+            return ["00"]
+
     if len(campo_minado[0]) == 3:
         if campo_minado[0][0] == ".":
             return ["000"]
--- test_campo_minado.py
+++ test_campo_minado.py
@@ -7,9 +7,11 @@ def test_uma_mina_retorna_uma_mina():
 def test_um_ponto_retorna_zero():
     assert resolve_campo_minado(["."]) == ["0"]
 
+def test_dois_pontos_retorna_zeros():
+    assert resolve_campo_minado([".."]) == ["00"]
+
 def test_linha_sem_mina_retorna_zeros():
     assert resolve_campo_minado(["..."]) == ["000"]
 
 def test_linha_com_mina_retorna_mina():
     assert resolve_campo_minado(["***"]) == ["***"]
-
