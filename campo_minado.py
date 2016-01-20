09441cf94d99d99f378832fe877133d0da4ceeb1
Dojo People <dojo@thoughtworks.com>
2016-01-20 20:30:40 -0200

    Teste com no maximo uma linha

--- campo_minado.py
+++ campo_minado.py
@@ -1,4 +1,10 @@
 def resolve_campo_minado(campo_minado):
-    if campo_minado == '.':
-        return '0'
+    if len(campo_minado[0]) == 3:
+        if campo_minado[0][0] == ".":
+            return ["000"]
+        else:
+            return["***"]
+
+    elif campo_minado == ['.']:
+        return ['0']
     return campo_minado
--- test_campo_minado.py
+++ test_campo_minado.py
@@ -2,7 +2,14 @@
 from campo_minado import resolve_campo_minado
 
 def test_uma_mina_retorna_uma_mina():
-    assert resolve_campo_minado("*") == "*"
+    assert resolve_campo_minado(["*"]) == ["*"]
 
 def test_um_ponto_retorna_zero():
-    assert resolve_campo_minado(".") == "0"
+    assert resolve_campo_minado(["."]) == ["0"]
+
+def test_linha_sem_mina_retorna_zeros():
+    assert resolve_campo_minado(["..."]) == ["000"]
+
+def test_linha_com_mina_retorna_mina():
+    assert resolve_campo_minado(["***"]) == ["***"]
+
